d876be361bad9a21b5eba4b387848edf92edae81
Michał Nowak <michal.nowak061@gmail.com>
2020-12-14 17:14:56 +0100

    Initial Commit

---
+++ ImageClassifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageClassifier
+//
+//  Created by Michał Nowak on 14/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
