2a56fc0ae6997338f6c6340e833e3f4d5b44018d
Michał Nowak <michal.nowak061@gmail.com>
2020-12-15 19:11:52 +0100

    PhotoPicker added

--- ImageClassifier/ViewControllers/ViewController.swift
+++ ImageClassifier/ViewControllers/ViewController.swift
@@ -9,11 +9,16 @@ import CoreML
 import ImageIO
 
 class ViewController: UIViewController {
-    var imageClassifierModel = ImageClassifierModel()
+    var imageClassifierModel: ImageClassifierModel!
+    var imagePicker: ImagePicker!
+    var loadedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+        
+        self.imageClassifierModel = ImageClassifierModel()
         do {
             let model = try MobileNet.init(configuration: MLModelConfiguration()).model
             imageClassifierModel.loadModel(input: model)
@@ -22,8 +27,24 @@ class ViewController: UIViewController {
         }
         
         if let image = UIImage(named: "banana1.jpg") {
-            imageClassifierModel.updateClassifications(for: image)
+            self.imageClassifierModel.updateClassifications(for: image)
         }
+    }
+    
+    // MARK: -- @IBOutle's
+    @IBOutlet weak var loadedImageView: UIImageView!
+    
+    // MARK: -- @IBAction's
+    @IBAction func loadButtonPressed(_ sender: UIButton) {
+        self.imagePicker.present(from: sender)
+        //self.loadedImage = UIImage(named: "banana1.jpg")
+        //self.loadedImageView.image = self.loadedImage
     }
 }
 
+extension ViewController: ImagePickerDelegate {
+
+    func didSelect(image: UIImage?) {
+        self.loadedImageView.image = image
+    }
+}
